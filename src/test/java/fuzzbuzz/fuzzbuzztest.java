855e0cc15e246f40b104c51ddc6ea80722df65a3
HP <amezouarasma4@gmail.com>
2024-12-19 10:10:41 +0100

    test@

--- src/test/java/fuzzbuzz/fuzzbuzztest.java
+++ src/test/java/fuzzbuzz/fuzzbuzztest.java
@@ -1,4 +1,17 @@
 package fuzzbuzz;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 public class fuzzbuzztest {
+
+    @Test
+    public  void finaltest(){
+String c="FUZZBUZZ";
+String reelly =FizzBuzz.de(15);
+assertEquals(c, reelly, "let's try it now ");
+
+    }
+
+
+
+
 }
